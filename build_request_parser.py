4657b69d9cc05a82b189db2ab6f4d9bd8ca429a0
bertfordley <rmosele4@vols.utk.edu>
2020-01-31 12:09:54 -0500

    update jupter notebook; add .py version

---
+++ build_request_parser.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+"""
+by: Robert C. Moseley
+"""
+import pandas as pd
+import argparse
+import sbol
+
+
+def sbh_login(SBH_USER, SBH_PASSWORD, spoof_bool, parts_doc):
+
+    if spoof_bool:
+        sbh_server = sbol.PartShop('https://hub-staging.sd2e.org')
+        sbh_server.spoof("https://hub.sd2e.org")
+        sbol.setHomespace('https://hub-staging.sd2e.org/user/sd2e/{collection}'.format(
+            collection=parts_doc.displayId))
+    else:
+        sbh_server = sbol.PartShop('https://hub.sd2e.org')
+        sbol.setHomespace('https://hub.sd2e.org/user/sd2e/{collection}'.format(
+            collection=parts_doc.displayId))
+
+    sbh_server.login(SBH_USER, SBH_PASSWORD)
+
+    return sbh_server
+
+
+def make_parts_doc(build_request):
+
+    parts_doc = sbol.Document()
+
+    collection_name = build_request.loc['Collection Name:'].iloc[:1][1].values[0]
+    parts_doc.name = collection_name
+    parts_doc.displayId = '_'.join(collection_name.split(' '))
+    collection_desc = build_request.loc['Design Description':].iloc[1:2].index[0]
+    parts_doc.description = collection_desc
+
+    return parts_doc
+
+
+def parse_parts_to_sbh(build_request, ontology_terms, parts_doc):
+
+    parts_df = build_request.loc['Part Name':'Composite DNA Parts']
+    parts_df.columns = parts_df.iloc[0]
+    parts_df = parts_df.drop(parts_df.index[0])
+    parts_df = parts_df.loc[parts_df.index.dropna()]
+    parts_df = parts_df.drop(['Composite DNA Parts'])
+
+    for part_name, part_info in parts_df.iterrows():
+        part_displayid = '_'.join(part_name.split(' '))
+        part_cd = sbol.ComponentDefinition(part_displayid, sbol.BIOPAX_DNA)
+        part_cd.name = part_name
+        part_cd.description = part_info['Description (Optional)']
+
+        if not pd.isnull(part_info['Role']):
+            if part_info['Role'] in ontology_terms.index:
+                role_uri = ontology_terms.loc[part_info['Role']].values[0]
+                part_cd.roles = part_cd.roles + [role_uri]
+
+        if not pd.isnull(part_info['Sequence']):
+            part_seq = sbol.Sequence('{}_sequence'.format(part_cd.displayId), part_info['Sequence'])
+            part_cd.sequence = part_seq
+
+        parts_doc.addComponentDefinition(part_cd)
+
+
+if __name__ == '__main__':
+
+    arg_parser = argparse.ArgumentParser(prog='Build Request Parser',
+                                         description='Parses Build Request excel document for DNA parts and submits to SynBioHub')
+
+    # - command-line arguments -
+    arg_parser.add_argument('-br', '--build_request_excel',
+                        action='store',
+                        required=True,
+                        help='full path to build request excel document')
+    arg_parser.add_argument('-u', '--sbh_username',
+                        action='store',
+                        required=True,
+                        help='SD2 SynBioHub username')
+    arg_parser.add_argument('-p', '--sbh_password',
+                        action='store',
+                        required=True,
+                        help='SD2 SynBioHub password')
+    arg_parser.add_argument('-s', '--spoof',
+                        action='store',
+                        type=bool,
+                        default=True,
+                        help='Default submits DNA parts to SynBioHub staging server. Set to False to submit to SynBioHub production server.')
+    args = arg_parser.parse_args()
+
+    path_to_build_request = args.build_request_excel
+    SBH_USER = args.sbh_username
+    SBH_PASSWORD = args.sbh_password
+    spoof_bool = args.spoof
+
+    # Load and parse build request
+    build_excel = pd.ExcelFile(path_to_build_request)
+    build_request = pd.read_excel(build_excel, 'Design', index_col=0, header=None)
+    ontology_terms = pd.read_excel(build_excel, 'Ontology Terms', index_col=0)
+
+    # create parts Document
+    parts_doc = make_parts_doc(build_request)
+
+    # connect to SBH server and set Homespace
+    sbh_server = sbh_login(SBH_USER, SBH_PASSWORD, spoof_bool, parts_doc)
+
+    # parse build request and add parts to parts Document
+    parse_parts_to_sbh(build_request, ontology_terms, parts_doc)
+
+    # submit parts Document to SBH server
+    sbh_server.submit(parts_doc, sbol.getHomespace(), 0)
